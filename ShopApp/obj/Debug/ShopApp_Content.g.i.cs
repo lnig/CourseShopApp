04bbc38afff78bd2b4a3f13bd122b248ca95aba7
Dominik Mierzejewski <dominik.mierzejewski1@wp.pl>
2024-05-09 23:09:41 +0200

    Dodanie widoku koszyka

--- ShopApp/obj/Debug/ShopApp_Content.g.i.cs
+++ ShopApp/obj/Debug/ShopApp_Content.g.i.cs
@@ -8,6 +8,11 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
+[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("assets/icons/cart.svg")]
+[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("assets/icons/credit_card.svg")]
+[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("assets/icons/delivery.svg")]
+[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("assets/icons/payment.svg")]
+[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("assets/icons/user.svg")]
 [assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("assets/illustrations/background.svg")]
 [assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("assets/illustrations/login.svg")]
 
