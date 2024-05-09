84218c41e45ff670ce7054aad7a06a490576cee8
RoyBig <kuba.ostaszewa@gmail.com>
2024-05-09 17:36:26 +0200

    Dodanie klasy kategoria oraz kurs

---
+++ ShopApp/Model/Category.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShopApp.Model
+{
+    public class Category
+    {
+        public int categoryId;
+        public int parentCategoryId;
+        public string name;
+
+        public Category(int categoryId, int parentCategoryId, string name)
+        {
+            this.categoryId = categoryId;
+            this.parentCategoryId = parentCategoryId;
+            this.name = name;
+        }
+    }
+}
---
+++ ShopApp/Model/Course.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShopApp.Model
+{
+    public class Course
+    {
+        private int courseId;
+        private int categoryId;
+        private string title;
+        private string author;
+        private string description;
+        private string longDescription;
+        private string prize;
+        private string imageTitle;
+
+        public Course (int courseId, string categoryId, string title, string author, string description, string longDescription, string prize, string imageTitle)
+        {
+            this.courseId = courseId;
+            this.categoryId = categoryId;
+            this.title = title;
+            this.author = author;
+            this.description = description;
+            this.longDescription = longDescription;
+            this.prize = prize;
+            this.imageTitle = imageTitle;
+        }
+    }
+}
