25e5c4a154fd4e9cc99af46594063f988f18632a
Dominik Mierzejewski <dominik.mierzejewski1@wp.pl>
2024-06-09 18:54:42 +0200

    Dodanie zapisywania w bazie order

---
+++ ShopApp/Model/Order.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShopApp.Model
+{
+    public class Order
+    {
+        [Key]
+        public int OrderId { get; set; }
+        public int UserId { get; set; }
+        public string FullName { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Address { get; set; }
+        public decimal TotalPrice { get; set; }
+        public DateTime OrderDate { get; set; }
+        public List<OrderCourse> OrderCourses { get; set; } = new List<OrderCourse>();
+
+        public Order() { }
+
+        public Order(int userId, string fullName, string phoneNumber, string address, decimal totalPrice, DateTime orderDate, List<Course> courses)
+        {
+            UserId = userId;
+            FullName = fullName;
+            PhoneNumber = phoneNumber;
+            Address = address;
+            TotalPrice = totalPrice;
+            OrderDate = orderDate;
+            OrderCourses = courses.Select(c => new OrderCourse { CourseId = c.CourseId }).ToList();
+        }
+    }
+}
--- ShopApp/ViewModel/CartViewModel.cs
+++ ShopApp/ViewModel/CartViewModel.cs
@@ -23,6 +23,7 @@ namespace ShopApp.ViewModel
     {
         private CartRepository cartRepository = new CartRepository();
         private CourseRepository courseRepository = new CourseRepository();
+        private OrderRepository orderRepository = new OrderRepository();
 
         public ObservableCollection<Course> UserCoursesInCart { get; set; }
 
@@ -30,6 +31,9 @@ namespace ShopApp.ViewModel
         private decimal _subTotal;
         private decimal _discount;
         private string _voucherCode;
+        private string _fullName;
+        private string _phoneNumber;
+        private string _address;
 
         public Cart SelectedCart
         {
@@ -72,18 +76,51 @@ namespace ShopApp.ViewModel
             }
         }
 
+        public string FullName
+        {
+            get { return _fullName; }
+            set
+            {
+                _fullName = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string PhoneNumber
+        {
+            get { return _phoneNumber; }
+            set
+            {
+                _phoneNumber = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string Address
+        {
+            get { return _address; }
+            set
+            {
+                _address = value;
+                OnPropertyChanged();
+            }
+        }
+
         public decimal Total => SubTotal - Discount;
 
         public ICommand RemoveFromCartCommand { get; private set; }
         public ICommand ApplyVoucherCommand { get; private set; }
+        public ICommand OrderCommand { get; private set; }
 
         public CartViewModel()
         {
             UserCoursesInCart = new ObservableCollection<Course>();
             LoadUserCoursesFromCart();
             SubTotal = CalculatePrice();
+            Discount = 0.00m;
             RemoveFromCartCommand = new RelayCommand<int>(RemoveFromCart);
             ApplyVoucherCommand = new RelayCommand(ApplyVoucher);
+            OrderCommand = new RelayCommand<object>(param => Order());
         }
 
         private int GetCurrentClientId()
@@ -136,19 +173,45 @@ namespace ShopApp.ViewModel
             return allPrice;
         }
 
+        private void Order()
+        {
+            Order order = new Order(GetCurrentClientId(), FullName, PhoneNumber, Address, Total, DateTime.Now, UserCoursesInCart.ToList());
+
+            if(order != null)
+            {
+                orderRepository.SaveOrder(order);
+
+                cartRepository.ClearCart(GetCurrentClientId());
+                UserCoursesInCart.Clear();
+
+                SubTotal = 0.00m;
+                PhoneNumber = String.Empty;
+                Address = String.Empty;
+                FullName = String.Empty;
+                VoucherCode = String.Empty;
+
+                OnPropertyChanged(nameof(SubTotal));
+                OnPropertyChanged(nameof(Total));
+
+                RefreshView();
+
+                MessageBox.Show("Order has been placed successfully and the cart has been cleared.");
+            }
+        }
+
         private void ApplyVoucher(object parameter)
         {
             if (VoucherCode == "DISCOUNT10")
             {
-                Discount = 10;
+                Discount = 10.00m;
             }
             else if (VoucherCode == "DISCOUNT15")
             {
-                Discount = 15;
+                Discount = 15.00m;
             }
             else
             {
-                Discount = 0;
+                Discount = 0.00m;
             }
             OnPropertyChanged(nameof(Discount));
             OnPropertyChanged(nameof(Total));
